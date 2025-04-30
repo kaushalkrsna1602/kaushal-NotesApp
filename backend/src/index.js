92488a2d9f77b8514bb4130b59993153312b5143
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-04-30 16:02:11 +0530

    cors updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,9 +12,15 @@ const { connectDB } = require('./lib/db.js');
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,               
+  'https://kaushal-notes-app.vercel.app/',
+      
+];
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({origin : allowedOrigins, credentials: true }));
 
 app.use("/auth" , authRoutes)
 app.use("/note" , noteRoutes)
