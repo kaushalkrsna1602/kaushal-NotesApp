4e1ecc135bc8376ea55c31540dccfbc9a60f6844
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-04-30 15:56:56 +0530

    cors updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 
 app.use("/auth" , authRoutes)
 app.use("/note" , noteRoutes)
